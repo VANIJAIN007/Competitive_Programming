7e1498cd39473aa94069df9e7bdb315ed55e49c5
VANIJAIN007 <vani.jain_cs19@gla.ac.in>
2022-01-26 21:30:16 -0800

    distinct Subsequence

---
+++ src/Class_programs/DistinctSubsequence.java
@@ -0,0 +1,28 @@
+package Class_programs;
+
+import java.util.Scanner;
+
+/*
+public class DistinctSubsequence
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String t=sc.next();
+        String s=sc.next();
+
+
+    }
+    public static void subSequence(String s,String t,int index)
+    {
+        int inc=0;
+        int exc=0;
+        if(s.charAt(index)==s.charAt(index))
+        {
+            inc=subSequence(s,t,index+!);
+        }
+         subSequence(s, t, index + 1);
+        return;
+    }
+}
+*/
